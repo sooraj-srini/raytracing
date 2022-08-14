69b85514683dc38748e1e56e2aac805d66ffe626
sooraj-srini <cs20b075@smail.iitm.ac.in>
2022-08-14 22:31:30 +0530

    Added progress line and simplified code a bit by using the Vector3 class from vecmath

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+use geometric::{vec3, Vector3};
+
+fn write_color(a:&Vector3<f64>) {
+    let color = 255.999;
+    println!("{} {} {}", (color*a.x()) as i32, (color*a.y()) as i32, (color*a.z()) as i32);
+}
+
 fn main() {
     let image_height = 256;
     let image_width = 256;
+    eprint!("hi");
     println!("P3\n{image_width} {image_height}\n255");
     for i in 0..image_height{
+        eprint!("\rScanning line {i}");
         for j in 0..image_width {
-            let r:f64 = j as f64/(image_height-1) as f64;
-            let g:f64 = i as f64/(image_width-1) as f64;
-            let b = 0.25;
-            let ir = (r*255.999) as i32;
-            let ig = (g*255.999) as i32;
-            let ib = (b*255.999) as i32;
-            println!("{ir} {ig} {ib}");
+            let rgb = vec3(j as f64/(image_height-1) as f64, i as f64/(image_width-1) as f64, 0.25);
+
+            write_color(&rgb);
         }
     }
 }
