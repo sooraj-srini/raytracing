6918b8190cae6b214aa4d2540687af1e4f2a6f35
sooraj-srini <cs20b075@smail.iitm.ac.in>
2022-08-19 21:28:06 +0530

    Added a basic camera

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+pub mod ray;
 use geometric::{vec3, Vector3};
+use ray::ray::Ray;
+
+fn ray_color(ray:&Ray) -> Vector3<f64> {
+    let u = ray.direction().norm();
+    let t = 0.5*(u.y() + 1.0);
+    return vec3(1.0, 1.0, 1.0)*(1.0 - t) + vec3(0.5, 0.7, 1.0)*t;
+}
+
 
 fn write_color(a:&Vector3<f64>) {
     let color = 255.999;
@@ -6,16 +15,32 @@ fn write_color(a:&Vector3<f64>) {
 }
 
 fn main() {
-    let image_height = 256;
-    let image_width = 256;
+    //Image
+    let image_aspect = 16.0/9.0;    
+    let image_width = 400;
+    let image_height = (image_width as f64/image_aspect) as i32;
+   
+    //Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = image_aspect*viewport_height;
+    let focal_length = 1.0;
+
+    let origin = vec3(0.0, 0.0, 0.0);
+    let horizontal = vec3(viewport_width, 0.0, 0.0);
+    let vertical = vec3(0.0, viewport_height, 0.0);
+    let lower_left = origin - horizontal/2.0 - vertical/2.0 - vec3(0.0, 0.0, focal_length);
+
     eprint!("hi");
     println!("P3\n{image_width} {image_height}\n255");
-    for i in 0..image_height{
-        eprint!("\rScanning line {i}");
+    for i in (0..image_height).rev() {
+        eprint!("\rScanning line {}", image_height-i);
         for j in 0..image_width {
-            let rgb = vec3(j as f64/(image_height-1) as f64, i as f64/(image_width-1) as f64, 0.25);
-
-            write_color(&rgb);
+            let u = (j as f64) / (image_width as f64 - 1.0);
+            let v = (i as f64) / (image_height as f64 - 1.0);
+            let ray = Ray{origin: origin, dir: lower_left + horizontal*u + vertical*v - origin};
+            let color = ray_color(&ray);
+            write_color(&color);
         }
     }
 }
--- src/ray.rs
+++ src/ray.rs
@@ -4,16 +4,19 @@ pub mod ray {
     type Type = Vector3<f64>;
 
     pub struct Ray {
-        origin: Type,
-        dir: Vector3<f64>,
+        pub origin: Type,
+        pub dir: Type,
     }
 
     impl Ray {
-        fn orig(&mut self) -> &Vector3<f64> {
+        pub fn orig(&mut self) -> &Vector3<f64> {
             &self.origin
         }
-        fn at(&mut self, t: f64) -> Vector3<f64> {
+        pub fn at(&mut self, t: f64) -> Vector3<f64> {
             self.origin + self.dir * t
+        }
+        pub fn direction(&self) -> Vector3<f64> {
+            self.dir.clone()
         }
     }
 }
